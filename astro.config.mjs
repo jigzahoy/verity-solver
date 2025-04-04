55e7e6edd523a86fdd1e5198dea7c3768c8a2259
Jigz <32875289+jigzahoy@users.noreply.github.com>
2025-04-04 09:31:38 +0800

    Add Github pages and fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jigzahoy.github.io',
+  base: 'verity-solver',
   vite: {
     plugins: [tailwindcss()],
   },
